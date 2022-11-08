1a47cd91ac0c1100e3e2ce9659ee7dbe96b5e1b5
Spacelnvader <la-douce-64@hotmail.fr>
2022-11-08 12:41:55 +0100

    supression table

--- src/components/TableData/index.tsx
+++ src/components/TableData/index.tsx
@@ -1,4 +1,4 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ChangeEvent, CSSProperties, Fragment, FunctionComponent, ReactNode, useEffect, useRef, useState } from 'react';
+
 import './style.scss';
 
